14d1c3af7d904b67b90ef443d5b3e7f0a87b6994
Drewnicorn15 <67112695+Drewnicorn15@users.noreply.github.com>
2025-02-24 14:45:27 +0000

    fr submission

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -1,6 +1,217 @@
 import java.lang.Comparable;
 
-public class BST implements BSTInterface
-{
+public class BST implements BSTInterface {
+  private TreeNode root;
 
+  public BST() {
+    root = null;
+  }
+
+  public int size() {
+    int size = 0;
+    if (root != null) {
+      size++;
+      size += sizehelper(root.getLeft());
+      size += sizehelper(root.getRight());
+      return size;
+    } else
+      return 0;
+  }
+
+  private int sizehelper(TreeNode subroot) {
+    int size = 0;
+    if (subroot != null) {
+      size++;
+      size += sizehelper(subroot.getLeft());
+      size += sizehelper(subroot.getRight());
+      return size;
+    } else
+      return 0;
+  }
+
+  public boolean find(Comparable toFind) {
+    if (root == null)
+      return false;
+    if (root.getValue().equals(toFind))
+      return true;
+    if (!findHelper(toFind, root.getLeft()) && !findHelper(toFind, root.getRight()))
+      return false;
+    else
+      return true;
+  }
+
+  public boolean findHelper(Comparable toFind, TreeNode subroot) {
+    if (subroot == null)
+      return false;
+    if (subroot.getValue().equals(toFind))
+      return true;
+    if (!findHelper(toFind, subroot.getLeft()) && !findHelper(toFind, subroot.getRight()))
+      return false;
+    else
+      return true;
+  }
+
+  public boolean replace(Comparable old, Comparable toAdd) {
+    if (find(old)) {
+      delete(old);
+      add(toAdd);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean isEmpty() {
+    if (root == null)
+      return true;
+    else
+      return false;
+  }
+
+  public void add(Comparable newVal) {
+    if (root == null)
+      root = new TreeNode(newVal);
+    else if (newVal.compareTo(root.getValue()) > 0)
+      if (root.getRight() == null)
+        root.setRight(new TreeNode(newVal));
+      else
+        addHelper(newVal, root.getRight());
+    else if (root.getLeft() == null)
+      root.setLeft(new TreeNode(newVal));
+    else
+      addHelper(newVal, root.getLeft());
+  }
+
+  private void addHelper(Comparable newVal, TreeNode subroot) {
+    if (subroot != null)
+      if (newVal.compareTo(subroot.getValue()) > 0)
+        if (subroot.getRight() == null)
+          subroot.setRight(new TreeNode(newVal));
+        else
+          addHelper(newVal, subroot.getRight());
+      else if (subroot.getLeft() == null)
+        subroot.setLeft(new TreeNode(newVal));
+      else
+        addHelper(newVal, subroot.getLeft());
+  }
+
+  public boolean delete(Comparable old) {
+    if (root != null) {
+      if (root.getValue().equals(old)) {
+        if (root.getRight() == null) {
+          root = root.getLeft();
+          root.setLeft(null);
+          root.setRight(null);
+          return true;
+        } else if (root.getLeft() == null) {
+          System.out.print(root.getRight().getValue());
+          root = root.getRight();
+          root.setLeft(null);
+          root.setRight(null);
+          return true;
+        } else {
+          TreeNode temp = root.getLeft();
+          while (temp.getRight() != null)
+            temp = temp.getRight();
+          temp.setRight(root.getRight());
+          root = root.getLeft();
+          return true;
+        } // endif
+      } else {
+        if (root.getValue().compareTo(old) < 0)
+          return deletehelper(root.getRight(), old, root);
+        else
+          return deletehelper(root.getLeft(), old, root);
+      } // endif
+    } else {
+      return false;
+    } // end if
+  }// end delete
+
+  private boolean deletehelper(TreeNode subroot, Comparable old, TreeNode parent) {
+    if (subroot != null) {
+      if (subroot.getValue().equals(old)) {
+        if (subroot.getRight() == null) {
+          if (parent.getLeft().equals(subroot))
+            parent.setLeft(subroot.getLeft());
+          else
+            parent.setRight(subroot.getLeft());
+          subroot.setLeft(null);
+          subroot.setRight(null);
+          return true;
+        } else if (subroot.getLeft() == null) {
+          if (parent.getLeft().equals(subroot))
+            parent.setLeft(subroot.getRight());
+          else
+            parent.setRight(subroot.getRight());
+          subroot.setLeft(null);
+          subroot.setRight(null);
+          return true;
+        } else {
+          TreeNode temp = subroot.getLeft();
+          while (temp.getRight() != null)
+            temp = temp.getRight();
+          temp.setRight(subroot.getRight());
+          subroot = subroot.getLeft();
+          return true;
+        } // end if
+
+      } else {
+        if (subroot.getValue().compareTo(old) < 0)
+          return deletehelper(subroot.getRight(), old, subroot);
+        else
+          return deletehelper(subroot.getLeft(), old, subroot);
+      } // end if
+    } else {
+      return false;
+    } // end if
+  }// end deletehelper
+
+  public void printInOrder() {
+    if (root != null) {
+      printInOrderHelper(root.getLeft());
+      System.out.println(root.getValue());
+      printInOrderHelper(root.getRight());
+    }
+  }
+
+  private void printInOrderHelper(TreeNode subroot) {
+    if (subroot != null) {
+      printInOrderHelper(subroot.getLeft());
+      System.out.println(subroot.getValue());
+      printInOrderHelper(subroot.getRight());
+    }
+  }
+
+  public void printPreOrder() {
+    if (root != null) {
+      System.out.println(root.getValue());
+      printPreOrderHelper(root.getLeft());
+      printPreOrderHelper(root.getRight());
+    }
+  }
+
+  private void printPreOrderHelper(TreeNode subroot) {
+    if (subroot != null) {
+      System.out.println(subroot.getValue());
+      printPreOrderHelper(subroot.getLeft());
+      printPreOrderHelper(subroot.getRight());
+    }
+  }
+
+  public void printPostOrder() {
+    if (root != null) {
+      printPostOrderHelper(root.getLeft());
+      printPostOrderHelper(root.getRight());
+      System.out.println(root.getValue());
+    }
+  }
+
+  private void printPostOrderHelper(TreeNode subroot) {
+    if (subroot != null) {
+      printPostOrderHelper(subroot.getLeft());
+      printPostOrderHelper(subroot.getRight());
+      System.out.println(subroot.getValue());
+    }
+  }
 }
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -1,14 +1,47 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * Drew Dietrich
+ * 2/24
+ * 3
+ * binary search tree progams
  */
 
- import java.lang.Comparable;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
- public class BSTTester {
-   public static void main(String[] args) {
-   
-	
- }
+// import org.junit.jupiter.api.Test;
+
+public class BSTTester {
+  public static void main(String[] args) {
+
+    System.out.println("Hello world!");
+    BST best = new BST();
+    System.out.println("is empty: " + best.isEmpty());
+    best.add("O");
+    best.add("R");
+    best.add("C");
+    best.add("H");
+    best.add("A");
+    best.add("R");
+    best.add("D");
+    best.add("Q");
+    best.add("U");
+    best.add("I");
+    best.add("N");
+    best.add("C");
+    best.add("E");
+    System.out.println("is empty: " + best.isEmpty());
+    System.out.print("in order traversal: ");
+    best.printInOrder();
+    System.out.println("find \"A\": " + best.find("A"));
+    System.out.println("replace \"Q\" with \"L\": "+ best.replace("Q", "L"));
+    System.out.print("post order traversal: ");
+    best.printPostOrder();
+    System.out.println("delete \"A\": " + best.delete("A"));
+    System.out.print("pre order traversal: ");
+    best.printPreOrder();
+  }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
+}
